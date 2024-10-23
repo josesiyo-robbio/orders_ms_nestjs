6855fe026ab1c2fa85dbbbf08ce00a54c26eb41f
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-10-22 19:04:10 -0600

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap() 
+{
+  const logger = new Logger('ORDERS-MS-MAIN');
+
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport : Transport.TCP,
+      options :
+      {
+        port : envs.port
+      }
+    }
+  );
+  
+
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+    );
+
+    await app.listen();
+    logger.log(`Products Micro Service running on PORT : ${envs.port}`)
 }
 bootstrap();
